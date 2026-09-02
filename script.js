d7d636b8f56c28448938343fef11e64cdca5a61b
JarciaCovane <covanejarcia9@gmail.com>
2026-09-02 05:53:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,26 +7,6 @@ function mostrarMenu() {
 }
 
 
-// Modo escuro
-
-const botaoTema = document.createElement("button");
-
-botaoTema.textContent = "Modo escuro";
-botaoTema.classList.add("botao-tema");
-
-document.querySelector("nav").appendChild(botaoTema);
-
-botaoTema.addEventListener("click", function() {
-    document.body.classList.toggle("modo-escuro");
-
-    if (document.body.classList.contains("modo-escuro")) {
-        botaoTema.textContent = "Modo claro";
-    } else {
-        botaoTema.textContent = "Modo escuro";
-    }
-});
-
-
 // Mensagem no console
 
 console.log("Portfólio de Járcia Nércia carregado com sucesso.");
